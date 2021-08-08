4519725ee37e2bd12fc99bdd4d12e88a0eeb27b2
Administrator <274337383@qq.com>
2021-08-08 13:49:32 +0800

    first

--- opencvpy.py
+++ opencvpy.py
@@ -8,13 +8,19 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = cv2.imread(r'Camera\1588918587421.jpg')  # 读取图像
-alipay = img[0:1080, 0:1680]  # ROI:保留你感兴趣的区域,W:1080  H:1680
+img = cv2.imread(r'lena1.jpeg')  # 读取图像
+# alipay = img[0:1080, 0:1680]  # ROI:保留你感兴趣的区域,W:1080  H:1680
 b, g, r = cv2.split(img)
 cur_img = img.copy()
 cur_img[:, :, 1] = 0  # R:0, 1    G:0, 2   B:1, 2
 cur_img[:, :, 2] = 0
 video = cv2.VideoCapture('D:\恢复\\FILE0080.MOV')  # 读取视频
+top_size, bottom_size, left_size, right_size = [50, 50, 50, 50]     # 边界填充
+replicate = cv2.copyMakeBorder(img, top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_REPLICATE)
+reflect = cv2.copyMakeBorder(img, top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_REFLECT)
+reflect101 = cv2.copyMakeBorder(img, top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_REFLECT101)
+wrap = cv2.copyMakeBorder(img, top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_WRAP)
+constant = cv2.copyMakeBorder(img, top_size, bottom_size, left_size, right_size, borderType=cv2.BORDER_CONSTANT)
 
 
 def cv_show(name, img):  # 显示图像的函数
@@ -46,9 +52,10 @@ def cv_ShowVideo():  # 播放视频函数
 
 
 if __name__ == '__main__':  # 这里是主函数(main),所有函数在这里调用执行.
-    cv_show('B', cur_img)
+    # cv_show('B', cur_img)
     # cv_show('G', cur_img)
     # cv_show('R', cur_img)
+    cv_show("lena", reflect)
     # cv_ShowVideo()
     # print(b.shape)
     # print(g.shape)
